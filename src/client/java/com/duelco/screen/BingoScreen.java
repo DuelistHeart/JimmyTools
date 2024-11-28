890937eb10169f1db024a42c4e4a991763be5f65
DuelistHeart <AshleyHarper2024@gmail.com>
2024-11-28 12:59:04 -0500

    Adding buttons for managing bingo cards.

--- src/client/java/com/duelco/screen/BingoScreen.java
+++ src/client/java/com/duelco/screen/BingoScreen.java
@@ -5,8 +5,11 @@ import io.wispforest.owo.ui.base.BaseOwoScreen;
 import io.wispforest.owo.ui.component.Components;
 import io.wispforest.owo.ui.container.Containers;
 import io.wispforest.owo.ui.container.FlowLayout;
+import io.wispforest.owo.ui.container.ScrollContainer;
 import io.wispforest.owo.ui.core.*;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.toast.SystemToast;
+import net.minecraft.client.toast.ToastManager;
 import net.minecraft.text.Text;
 import org.jetbrains.annotations.NotNull;
 
@@ -18,7 +21,7 @@ public class BingoScreen extends BaseOwoScreen<FlowLayout> {
 
     @Override
     protected @NotNull OwoUIAdapter<FlowLayout> createAdapter() {
-        return OwoUIAdapter.create(this, Containers::horizontalFlow);
+        return OwoUIAdapter.create(this, Containers::verticalFlow);
     }
 
     @Override
@@ -28,12 +31,6 @@ public class BingoScreen extends BaseOwoScreen<FlowLayout> {
 
     @Override
     protected void build(FlowLayout rootComponent) {
-        if (bingoCards.isEmpty()) {
-            bingoCards.add(new BingoCard());
-            bingoCards.add(new BingoCard());
-            bingoCards.add(new BingoCard());
-        }
-
         rootComponent
                 .surface(Surface.VANILLA_TRANSLUCENT)
                 .alignment(HorizontalAlignment.CENTER, VerticalAlignment.CENTER);
@@ -45,15 +42,40 @@ public class BingoScreen extends BaseOwoScreen<FlowLayout> {
             bingoCardsLayout.child(bingoCard.buildBingoCardComponent());
         }
 
-        rootComponent.child(
-                bingoCardsLayout
-        ).child(
-                Components.button(Text.of("Clear cards"), buttonComponent -> {
+        FlowLayout buttonGroup = (FlowLayout) Containers.horizontalFlow(Sizing.fill(), Sizing.fixed(50))
+                .horizontalAlignment(HorizontalAlignment.CENTER);
+
+        buttonGroup.child(
+                Components.button(Text.of("Clear Cards"), buttonComponent -> {
                     for (BingoCard bingoCard : bingoCards) {
                         bingoCard.clear();
                         MinecraftClient.getInstance().setScreen(new BingoScreen());
                     }
-                })
+                }).margins(Insets.of(2))
+        ).child(
+                Components.button(Text.of("Generate Card"), buttonComponent -> {
+                    if (bingoCards.size() < 3) {
+                        bingoCards.add(new BingoCard());
+                        MinecraftClient.getInstance().setScreen(new BingoScreen());
+                    } else {
+                        MinecraftClient.getInstance().getToastManager().add(new SystemToast(
+                                SystemToast.Type.CHUNK_SAVE_FAILURE,
+                                Text.of("Max Cards Reached"),
+                                Text.of("Max cards currently set to 3")
+                        ));
+                    }
+                }).margins(Insets.of(2))
+        ).child(
+                Components.button(Text.of("Reset"), buttonComponent -> {
+                    bingoCards.clear();
+                    MinecraftClient.getInstance().setScreen(new BingoScreen());
+                }).margins(Insets.of(2))
+        );
+
+        rootComponent.child(
+                bingoCardsLayout
+        ).child(
+                buttonGroup
         );
     }
 }
