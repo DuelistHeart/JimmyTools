3d78fa4c4d3dae511fd432887a27a7da0e361e64
DuelistHeart <AshleyHarper2024@gmail.com>
2024-11-29 21:53:20 -0500

    Adding support for custom marker placement, rather than static.

--- src/client/java/com/duelco/ui/managers/BingoItemUIManager.java
+++ src/client/java/com/duelco/ui/managers/BingoItemUIManager.java
@@ -1,12 +1,10 @@
 package com.duelco.ui.managers;
 
 import com.duelco.obj.BingoItem;
-import com.duelco.ui.screen.BingoScreen;
 import io.wispforest.owo.ui.component.Components;
 import io.wispforest.owo.ui.container.Containers;
 import io.wispforest.owo.ui.container.StackLayout;
 import io.wispforest.owo.ui.core.*;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.util.Identifier;
 
 public class BingoItemUIManager {
@@ -26,19 +24,14 @@ public class BingoItemUIManager {
             sampleSlot.child(
                     Components.texture(Identifier.of("duelutils", "ui/free_space.png"), 1, 1, 256, 256)
                             .sizing(Sizing.fixed(20))
-                            .zIndex(20)
+                            .zIndex(500)
             );
         } else {
             sampleSlot.child(
                     Components.item(bingoItem.getItem())
+                            .zIndex(25)
             );
         }
-
-        sampleSlot.mouseDown().subscribe((a, b, c) -> {
-            bingoItem.setMarked(!bingoItem.isMarked());
-            MinecraftClient.getInstance().setScreen(new BingoScreen());
-            return true;
-        });
 
         if (bingoItem.isMarked()) {
             sampleSlot.child(
--- src/client/java/com/duelco/ui/screen/BingoScreen.java
+++ src/client/java/com/duelco/ui/screen/BingoScreen.java
@@ -3,10 +3,12 @@ package com.duelco.ui.screen;
 import com.duelco.managers.BingoManager;
 import com.duelco.obj.BingoCard;
 import com.duelco.ui.managers.BingoCardUIManager;
+import com.duelco.ui.managers.BingoMarkerUIManager;
 import io.wispforest.owo.ui.base.BaseOwoScreen;
 import io.wispforest.owo.ui.component.Components;
 import io.wispforest.owo.ui.container.Containers;
 import io.wispforest.owo.ui.container.FlowLayout;
+import io.wispforest.owo.ui.container.StackLayout;
 import io.wispforest.owo.ui.core.*;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.text.Text;
@@ -14,6 +16,7 @@ import org.jetbrains.annotations.NotNull;
 
 public class BingoScreen extends BaseOwoScreen<FlowLayout> {
     private static final BingoManager bingoManager = new BingoManager();
+    private static final BingoMarkerUIManager bingoMarkerManager = new BingoMarkerUIManager();
 
     @Override
     protected @NotNull OwoUIAdapter<FlowLayout> createAdapter() {
@@ -31,8 +34,20 @@ public class BingoScreen extends BaseOwoScreen<FlowLayout> {
                 .surface(Surface.VANILLA_TRANSLUCENT)
                 .alignment(HorizontalAlignment.CENTER, VerticalAlignment.CENTER);
 
+        StackLayout bingoCardsAndMarkerLayout = (StackLayout) Containers.stack(Sizing.fill(), Sizing.fixed(220))
+                .alignment(HorizontalAlignment.CENTER, VerticalAlignment.CENTER);
+
         FlowLayout bingoCardsLayout = (FlowLayout) Containers.horizontalFlow(Sizing.fill(), Sizing.fixed(220))
                 .alignment(HorizontalAlignment.CENTER, VerticalAlignment.CENTER);
+
+        bingoCardsAndMarkerLayout.child(bingoCardsLayout)
+                .child(bingoMarkerManager.bingoMarkersLayout);
+
+        bingoCardsLayout.mouseDown().subscribe((x, y, btn) -> {
+                    bingoMarkerManager.addMarker((int) x, (int) y);
+
+                    return true;
+                });
 
         for (BingoCard bingoCard : bingoManager.getCards()) {
             bingoCardsLayout.child(BingoCardUIManager.buildBingoCardComponent(bingoCard));
@@ -43,7 +58,7 @@ public class BingoScreen extends BaseOwoScreen<FlowLayout> {
 
         buttonGroup.child(
                 Components.button(Text.of("Clear Cards"), buttonComponent -> {
-                    bingoManager.clearMarksFromCards();
+                    bingoMarkerManager.clearMarkers();
                     MinecraftClient.getInstance().setScreen(new BingoScreen());
                 }).margins(Insets.of(2))
         ).child(
@@ -61,7 +76,7 @@ public class BingoScreen extends BaseOwoScreen<FlowLayout> {
         rootComponent.child(
                 Components.label(Text.of("Bingo Screen"))
         ).child(
-                bingoCardsLayout
+                bingoCardsAndMarkerLayout
         ).child(
                 buttonGroup
         );
