b041ccdafc2c04ae1669e436035ce6c752520e30
DuelistHeart <AshleyHarper2024@gmail.com>
2024-12-30 04:20:04 -0500

    Adding support for persisting bingo card marks between disconnects/crashes.

--- src/client/java/com/duelco/obj/DataStore.java
+++ src/client/java/com/duelco/obj/DataStore.java
@@ -1,16 +1,17 @@
 package com.duelco.obj;
 
 import com.duelco.managers.BingoManager;
-import com.duelco.managers.DataManager;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class DataStore {
     public List<BingoCard> bingoCards;
+    private List<UiPosition> bingoMarkerPositions;
 
     public DataStore() {
         this.bingoCards = new ArrayList<>();
+        this.bingoMarkerPositions = new ArrayList<>();
     }
 
     public void setBingoCards(List<BingoCard> bingoCards) {
@@ -21,7 +22,19 @@ public class DataStore {
         return bingoCards;
     }
 
+    public void addBingoMarkerPosition(UiPosition markerPos) {
+        this.bingoMarkerPositions.add(markerPos);
+    }
+
+    public void removeBingoMarkerPosition(UiPosition markerPos) {
+        this.bingoMarkerPositions.remove(markerPos);
+    }
+
     public void unloadData() {
         BingoManager.loadCards(bingoCards);
+    }
+
+    public List<UiPosition> getBingoMarkerPositions() {
+        return bingoMarkerPositions;
     }
 }
--- src/client/java/com/duelco/ui/managers/BingoMarkerUIManager.java
+++ src/client/java/com/duelco/ui/managers/BingoMarkerUIManager.java
@@ -1,36 +1,65 @@
 package com.duelco.ui.managers;
 
+import com.duelco.managers.DataManager;
+import com.duelco.obj.DataStore;
+import com.duelco.obj.UiPosition;
 import io.wispforest.owo.ui.component.Components;
 import io.wispforest.owo.ui.container.Containers;
 import io.wispforest.owo.ui.container.FlowLayout;
 import io.wispforest.owo.ui.core.*;
 import net.minecraft.util.Identifier;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BingoMarkerUIManager {
     private static final int MARKER_SIZE = 20;
     private static final int MARKER_INDEX = 30;
 
+    private static final List<UiPosition> bingoMarkerPositions = new ArrayList<>();
     public final FlowLayout bingoMarkersLayout;
 
     public BingoMarkerUIManager() {
         this.bingoMarkersLayout =  (FlowLayout) Containers.horizontalFlow(Sizing.fill(), Sizing.fixed(220))
                 .alignment(HorizontalAlignment.CENTER, VerticalAlignment.CENTER);
+
+        if (DataManager.getDataStore().getBingoMarkerPositions() != null && !DataManager.getDataStore().getBingoMarkerPositions().isEmpty()) {
+            for (UiPosition markerPos : DataManager.getDataStore().getBingoMarkerPositions()) {
+                bingoMarkerPositions.add(markerPos);
+                addMarkerToComponent(markerPos);
+            }
+        }
     }
 
     public void addMarker(int x, int y) {
-        Component markerComponent = getMarkerComponent().positioning(Positioning.absolute(x-(MARKER_SIZE/2), y-(MARKER_SIZE/2)));
+        UiPosition markerPos = new UiPosition(x-(MARKER_SIZE/2), y-(MARKER_SIZE/2));
+        addMarkerToComponent(markerPos);
+        bingoMarkerPositions.add(markerPos);
+        DataManager.getDataStore().addBingoMarkerPosition(markerPos);
+        DataManager.saveData();
+    }
+
+    public void clearMarkers() {
+        bingoMarkersLayout.clearChildren();
+        bingoMarkerPositions.clear();
+        DataManager.getDataStore().getBingoMarkerPositions().clear();
+        DataManager.saveData();
+    }
+
+    private void addMarkerToComponent(UiPosition markerPos) {
+        Component markerComponent = getMarkerComponent().positioning(Positioning.absolute(markerPos.getX(), markerPos.getY()));
+
         markerComponent.mouseDown().subscribe((mx, my, btn) -> {
+            bingoMarkerPositions.remove(markerPos);
+            DataManager.getDataStore().removeBingoMarkerPosition(markerPos);
             markerComponent.remove();
+            DataManager.saveData();
             return true;
         });
 
         bingoMarkersLayout.child(
                 markerComponent
         );
-    }
-
-    public void clearMarkers() {
-        bingoMarkersLayout.clearChildren();
     }
 
     private Component getMarkerComponent() {
